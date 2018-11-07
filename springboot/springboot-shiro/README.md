bae5eed9b335b660a2bef152d51abc6e5bd1beca
staconfree@163.com <Kingdom1>
2018-11-07 17:57:11 +0800

    springboot.shiro

---
+++ springboot/springboot-shiro/README.md
@@ -0,0 +1,2 @@
+#参考
+https://github.com/ityouknow/spring-boot-examples
---
+++ springboot/springboot-shiro/src/main/java/com/roy/github/learn/springboot/shiro/SpringBootShiroApplication.java
@@ -0,0 +1,8 @@
+package com.roy.github.learn.springboot.shiro;
+
+/**
+ * Created by roy on 2018/11/7.
+ */
+public class SpringBootShiroApplication {
+
+}
